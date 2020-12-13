dfabbef3fd64fc49009bc4fbef552645d663c641
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-13 19:04:34 +0100

    another try

--- Build/Azure/scripts/firebird4.sh
+++ Build/Azure/scripts/firebird4.sh
@@ -4,7 +4,7 @@ docker ps -a
 sleep 15
 
 # create Dialect1 database
-docker exec firebird echo "SET SQL DIALECT 1;CREATE DATABASE '/firebird/data/testdbd1.fdb' USER 'SYSDBA' PASSWORD 'masterkey' DEFAULT CHARACTER SET UTF8;QUIT;" | /usr/local/firebird/bin/isql
+docker exec -i firebird echo "SET SQL DIALECT 1;CREATE DATABASE '/firebird/data/testdbd1.fdb' USER 'SYSDBA' PASSWORD 'masterkey' DEFAULT CHARACTER SET UTF8;QUIT;" | /usr/local/firebird/bin/isql
 
 docker exec firebird ls -a /firebird/data
 docker exec firebird ls -a /firebird/log
